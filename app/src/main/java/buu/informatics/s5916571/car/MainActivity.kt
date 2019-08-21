8bbc59cbb82b1ff6ab7c1209b68e534a4338ab82
tee29909 <59160571@go.buu.ac.th>
2019-08-21 18:22:47 +0700

    park

---
+++ app/src/main/java/buu/informatics/s5916571/car/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.informatics.s5916571.car
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
